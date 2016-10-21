0bf718134f06db446691ed33a993d1beecba07d6
Baptiste Fontaine <b@ptistefontaine.fr>
2016-10-21 23:19:35 +0200

    Add leonard/ pkg with Binary

---
+++ leonard/grayscale.go
@@ -0,0 +1,63 @@
+package leonard
+
+import (
+	"image"
+	"image/color"
+)
+
+var (
+	black = color.Gray16{0}
+	white = color.Gray16{0xffff}
+)
+
+func luminance(r, g, b uint32) float32 {
+	// Ref:
+	// https://en.wikipedia.org/wiki/Grayscale#Converting_color_to_grayscale
+	return 0.2126*float32(r) + 0.7152*float32(g) + 0.0722*float32(b)
+}
+
+func grayscale(r, g, b, a uint32) uint16 {
+	alpha := float32(a) / 0xffff
+	linear := luminance(r, g, b) * alpha
+
+	return uint16(linear)
+}
+
+// Convert a colored image to a grayscaled one
+func Grayscale(img image.Image) image.Image {
+	grayscaled := image.NewRGBA(img.Bounds())
+
+	bd := img.Bounds()
+	for y := bd.Min.Y; y < bd.Max.Y; y++ {
+		for x := bd.Min.X; x < bd.Max.X; x++ {
+			r, g, b, a := img.At(x, y).RGBA()
+			grayscaled.Set(x, y, color.Gray16{grayscale(r, g, b, a)})
+		}
+	}
+
+	return grayscaled
+}
+
+func Binary(img image.Image) image.Image {
+	return BinaryWithThreshold(img, uint16(0xffff*7/10))
+}
+
+func BinaryWithThreshold(img image.Image, threshold uint16) image.Image {
+	binary := image.NewRGBA(img.Bounds())
+
+	bd := img.Bounds()
+	for y := bd.Min.Y; y < bd.Max.Y; y++ {
+		for x := bd.Min.X; x < bd.Max.X; x++ {
+			r, g, b, a := img.At(x, y).RGBA()
+			gray := grayscale(r, g, b, a)
+
+			if gray >= threshold {
+				binary.Set(x, y, black)
+			} else {
+				binary.Set(x, y, white)
+			}
+		}
+	}
+
+	return binary
+}
--- main.go
+++ main.go
@@ -3,31 +3,13 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/color"
 	"image/png"
 	"io"
 	"log"
 	"os"
+
+	"github.com/bfontaine/leonard/leonard"
 )
-
-func toGrayscale(img image.Image) image.Image {
-	grayscaled := image.NewRGBA(img.Bounds())
-
-	bd := img.Bounds()
-	for y := bd.Min.Y; y < bd.Max.Y; y++ {
-		for x := bd.Min.X; x < bd.Max.X; x++ {
-			// https://en.wikipedia.org/wiki/Grayscale#Converting_color_to_grayscale
-			r, g, b, _ := img.At(x, y).RGBA()
-			linear := 0.2126*float32(r) + 0.7152*float32(g) + 0.0722*float32(b)
-
-			y_linear := uint16(linear)
-
-			grayscaled.Set(x, y, color.Gray16{y_linear})
-		}
-	}
-
-	return grayscaled
-}
 
 func writePNG(img image.Image, w io.Writer) error {
 	return png.Encode(w, img)
@@ -50,7 +32,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	gray := toGrayscale(img)
+	transformed := leonard.Binary(img)
 
 	out, err := os.Create("out.png")
 	if err != nil {
@@ -58,7 +40,7 @@ func main() {
 	}
 	defer out.Close()
 
-	if err = writePNG(gray, out); err != nil {
+	if err = writePNG(transformed, out); err != nil {
 		log.Fatal(err)
 	}
 }
